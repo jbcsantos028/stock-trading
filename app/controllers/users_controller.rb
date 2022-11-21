cdd293e18aa21c7949e659d163e5746a8844b4e1
jbcsantos028 <jbcsantos028@gmail.com>
2022-11-21 11:48:34 +0800

    refactor(_list.html.erb): changed the querying of shares from the UserStock table

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def my_portfolio
-    @owned_stocks = current_user.stocks
+    @owned_stocks = current_user.user_stocks
     # @owned_stocks = current_user.user_stocks
   end
 end
