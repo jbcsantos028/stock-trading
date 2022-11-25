68824a4dec335c1c6028e23842d55494a47337e8
germeeron <germeeron@gmail.com>
2022-11-25 20:57:38 +0800

    admin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,39 @@
 class UsersController < ApplicationController
+  before_action :only_admin, only: :adminpanel
+
+  def adminpanel
+    @users = User.all
+  end
+
+  # GET /users/new
+  def new
+    if current_user.admin?
+      @user = User.new
+    end
+  end
+
+  # POST /users
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+        redirect_to adminpanel_path
+    else
+        render :new
+    end
+end
+
   
+
+  private
+
+  def only_admin
+    if current_user.nil? || current_user.admin == false
+      redirect_to root_path
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   resources :transaction_records, only: [:index]
   resources :transactions, only: [:create, :update]
+  resources :users
   devise_for :users
   root to: 'transactions#index'
   get 'get_stock_quote', to: 'stocks#get_quote'
   get 'edit_transaction', to: 'transactions#edit'
   get 'my_portfolio', to: 'transactions#index'
+  
+  get 'adminpanel', to: 'users#adminpanel'
 end
