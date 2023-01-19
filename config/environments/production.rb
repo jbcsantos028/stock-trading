e5f619d6301a4ee1d29311b717b3e34ac2996c73
jbcsantos028 <jbcsantos028@gmail.com>
2023-01-19 17:13:21 +0800

    Initial setup for deploying to Railway

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,7 +5,19 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
-
+  
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = {host: "", protocol: "https"}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    user_name:      Rails.application.credentials.dig(:google_smtp, :email),
+    password:       Rails.application.credentials.dig(:google_smtp, :password),
+    domain:         "gmail.com",
+    address:       "smtp.gmail.com",
+    port:          "587",
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
@@ -66,7 +78,7 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
